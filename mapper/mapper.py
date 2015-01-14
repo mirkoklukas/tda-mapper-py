83354d72416dbd1c1c50199afbcc4b5c7948c672
mirkok <mirko.klukas@gmail.com>
2015-01-14 17:37:22 +0100

    File renamed.

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-# A straighforward implementation of the mapper construction by Carlsson--Memoli--Singh.
+# A straightforward implementation of the mapper construction by Carlsson--Memoli--Singh.
 # (I wrote a little blog post about it at http://mapsfunctionsandarrows.blogspot.de)
 # ------------------------------------------------------
 
