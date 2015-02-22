9045f02bc6c2a8f0a23ac4d2bc6b79c213f5ac27
mirkok <mirko.klukas@gmail.com>
2015-02-22 14:37:14 +0100

    changed dataset image

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='tdamapper',
       version='0.1',
       description='A straight-forward implementation of the mapper construction by Carlsson--Memoli--Singh.',
-      url='https://github.com/mirkoklukas/mapper-py',
+      url='https://github.com/mirkoklukas/tda-mapper-py',
       author='Mirko Klukas',
       author_email='mirko.klukas@gmail.com',
       license='MIT',
